8603d236a9f81ec7a068d64f7124ae340cb0d99a
“Volodia” <nxdgbccvbnnnn@gmail.com>
2023-11-11 12:23:56 +0100

    chore: initial backend setup

---
+++ src/main/java/com/example/flowersproject/FlowersProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowersproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowersProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowersProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowersproject/FlowersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
