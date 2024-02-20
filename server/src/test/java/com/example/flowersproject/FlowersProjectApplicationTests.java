1a8953ec0e862faf991a4a63310c9aef6ba51d33
soso4ok <vorobiovvolodymyr2005@gmail.com>
2024-02-20 15:07:59 +0100

    change git ignore config

--- server/src/test/java/com/example/flowersproject/FlowersProjectApplicationTests.java
+++ server/src/test/java/com/example/flowersproject/FlowersProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.flowersproject;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlowersProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
