b5ce8d69a2d7da2403043363e85f8408979eada8
soso4ok <nxdgbccvbnnnn@gmail.com>
2025-09-15 14:39:15 +0200

    feat: get admin user from credentials

--- src/main/java/com/example/flowersproject/FlowersProjectApplication.java
+++ src/main/java/com/example/flowersproject/FlowersProjectApplication.java
@@ -1,5 +1,6 @@
 package com.example.flowersproject;
 
+import com.example.flowersproject.config.AdminProperties;
 import com.example.flowersproject.dto.UserDTO;
 import com.example.flowersproject.entity.user.UserEntity;
 import com.example.flowersproject.entity.user.UserRole;
@@ -9,11 +10,13 @@ import com.example.flowersproject.services.impl.UserServiceImpl;
 import com.example.flowersproject.services.impl.auth.AuthenticationService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
@@ -22,23 +25,22 @@ public class FlowersProjectApplication implements CommandLineRunner {
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
+    private final AdminProperties adminProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowersProjectApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		UserEntity userEntity = userRepository.findByRole(UserRole.ADMIN);
-		if (userEntity == null) {
-			userRepository.save(UserEntity.builder()
-								.firstname("admin")
-								.lastname("admin")
-								.email("admin@gmail.com")
-								.password(passwordEncoder.encode("admin"))
-								.role(UserRole.ADMIN)
-								.build()
-			);
-		}
-	}
+    @Override
+    public void run(String... args) {
+        if (userRepository.findByEmail(adminProperties.getEmail()).isEmpty()) {
+            userRepository.save(UserEntity.builder()
+                    .firstname("admin")
+                    .lastname("admin")
+                    .email(adminProperties.getEmail())
+                    .password(passwordEncoder.encode(adminProperties.getPassword()))
+                    .role(UserRole.ADMIN)
+                    .build());
+        }
+    }
 }
